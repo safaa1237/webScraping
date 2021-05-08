c22b883bb52a999647a433935f5771b9b956a358
safaa1237 <41582021+safaa1237@users.noreply.github.com>
2021-05-08 20:13:10 +0200

    finished

--- engine/src/title/title.service.ts
+++ engine/src/title/title.service.ts
@@ -5,8 +5,8 @@ import {sendAsinToQueue , recieveTitle} from '../../queue/titleQueue'
 @Injectable()
 export class TitleService {
 
-    getTitleFromQueue(ison){
-        sendAsinToQueue(ison);
+    getTitleFromQueue(asin){
+        sendAsinToQueue(asin);
         let title = recieveTitle();
         return title;
     }
